20dc200db122f2be441b9d267c90861f369a9f8a
Neil Glikin <glikin@berkeley.edu>
2022-07-20 14:18:58 -0700

    Add parameter for number of ions in Heating_Rate_Rabi experiment
    This allows for calculation of Lamb-Dicke factor to include number of ions, not just the trap frequency (doesn't affect functionality of experiment).

--- scripts/scripts_v2/Heating_Rate_Rabi.py
+++ scripts/scripts_v2/Heating_Rate_Rabi.py
@@ -12,12 +12,14 @@ class Rabi(Excitation729):
     def run_finally(cls, cxn, parameter_dict, data, data_x):
         data_y = data.sum(1)
         trap_frequency_MHz = parameter_dict.HeatingRateRabi.trap_frequency['MHz']
-        nbar = cls.rabi_fit(data_x, data_y, trap_frequency_MHz, return_all_params = False)
+        n_ions = parameter_dict.HeatingRateRabi.n_ions
+        nbar = cls.rabi_fit(data_x, data_y, trap_frequency_MHz, n_ions, return_all_params = False)
         return nbar
 
 
 class Heating_Rate_Rabi(pulse_sequence):
     show_params = ['HeatingRateRabi.trap_frequency',
+                   'HeatingRateRabi.n_ions'
                     ]
 
     scannable_params = {'EmptySequence.empty_sequence_duration': [(0., 40., 10., 'ms'), 'nbar']}
