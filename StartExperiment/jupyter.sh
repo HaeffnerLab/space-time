688635058fbfc83c077ec7c1916d1587a52f3cf6
Neil Glikin <glikin@berkeley.edu>
2024-04-11 16:53:35 -0700

    Misc convenience changes

--- StartExperiment/jupyter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/virtual-envs/jupyter-env/bin/activate
-cd /home/space-time/Science
-jupyter notebook
--- StartExperiment/labrad.sh
+++ StartExperiment/labrad.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-/home/space-time/LabRAD/scalabrad-0.7.2/bin/labrad --tls-required=false &
+~/LabRAD/scalabrad/scalabrad-0.7.2/bin/labrad --tls-required=false &
 
-~/LabRAD/scalabrad-web-server-1.1.0/bin/labrad-web &
+~/LabRAD/scalabrad/scalabrad-web-server-1.1.0/bin/labrad-web &
 
 source ~/virtual-envs/labrad-env/bin/activate
 python -m labrad.node
+
